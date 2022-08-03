bcc3f314a533051052a2901eeaf45f1805f60cd2
Hythraux <86388177+Hythrex@users.noreply.github.com>
2022-08-03 13:58:14 -0400

    Update files

--- mods/True Sun God Tower/Main.cs
+++ mods/True Sun God Tower/Main.cs
@@ -1,20 +1,47 @@
 ﻿using BTD_Mod_Helper;
 using MelonLoader;
-using Harmony;
+using HarmonyLib;
 using Assets.Scripts.Simulation.Towers;
 using Assets.Scripts.Models;
+using Assets.Scripts.Models.Towers;
+using Assets.Scripts.Utils;
+using Assets.Scripts.Unity;
+using BTD_Mod_Helper.Api.Towers;
+using BTD_Mod_Helper.Extensions;
+using Il2CppSystem.Collections.Generic;
+using UnhollowerBaseLib;
 
-[assembly: MelonInfo(typeof(TemplateMod.Main), "Your Mod Name", "1.0.0", "Your Name")]
+[assembly: MelonInfo(typeof(True_Sun_God_Tower.TemplateMain), "True Sun God in shop", "0.0.1", "Hythrex")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
-namespace TemplateMod
+
+namespace True_Sun_God_Tower
 {
-    public class Main : BloonsTD6Mod
-    {
-        public override void OnApplicationStart()
+    public class TemplateMain : BloonsTD6Mod
+    { 
+        public override void OnMainMenu()
         {
-            base.OnApplicationStart();
-            MelonLogger.Msg("ding");
+            base.OnMainMenu();
         }
 
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("Mod Loaded.");
+        }
+    }
+
+    [HarmonyPatch(typeof(Tower), "Initialise")]
+
+    public class TowerInitialise_Patch
+    {
+        [HarmonyPrefix]
+
+        public static bool Prefix(Tower __instance, ref Model modelToUse)
+        {
+            if (!modelToUse.name.Contains("SunMonkey"))
+                return true;
+
+            modelToUse = Game.instance.model.GetTower(TowerType.SuperMonkey, 5, 2, 0); // get true sun god model
+            return true;
+        }
     }
 }
---
+++ mods/True Sun God Tower/SunMonkey.cs
@@ -0,0 +1,54 @@
+﻿// imports
+
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Models;
+using Assets.Scripts.Models.Towers;
+using Assets.Scripts.Models.TowerSets;
+using Assets.Scripts.Utils;
+using Assets.Scripts.Unity;
+using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.Towers;
+using BTD_Mod_Helper.Extensions;
+using Il2CppSystem.Collections.Generic;
+using Assets.Scripts.Simulation.Towers;
+using UnhollowerBaseLib;
+using MelonLoader;
+using HarmonyLib;
+
+namespace True_Sun_God_Tower
+{
+    public class SunMonkey : ModTower
+    {
+        public override SpriteReference IconReference => Game.instance.model.GetTowerWithName("SuperMonkey").icon; // get supermonkey's stuff
+
+        public override SpriteReference PortraitReference => Game.instance.model.GetTowerWithName("SuperMonkey").portrait;
+        public override string BaseTower => TowerType.SuperMonkey;
+        public override int TopPathUpgrades => 0;
+
+        public override int MiddlePathUpgrades => 0;
+
+        public override int BottomPathUpgrades => 0;
+
+        public override string Description => "Shoots powerful beams to destroy any bloon that gets near it. It i extremely useful for MOAB class type bloons.";
+
+        public override string TowerSet => MAGIC;
+
+        public override string DisplayName => "True Sun God";
+
+        public override int Cost => 600;
+
+        public override void ModifyBaseTowerModel(TowerModel towerModel)
+        {
+            towerModel.radius = 6;
+            towerModel.radiusSquared = 36;
+            foreach (var weaponModel in towerModel.GetWeapons())
+            {
+                weaponModel.Rate = 0; // makes it go incredible fast
+                /* basically the code that makes hypersonic mods :/ 
+                   yeah im not that creative
+                */
+            }
+        }
+    }
+}
