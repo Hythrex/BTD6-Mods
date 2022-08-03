dd56b2c19d5e9c446ba88dedeb0c88e7645628d4
Hythraux <86388177+Hythrex@users.noreply.github.com>
2022-08-03 14:04:36 -0400

    Added file(s)

---
+++ mods/TemplateMod/Main.cs
@@ -0,0 +1,23 @@
+﻿using BTD_Mod_Helper;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(TemplateMod.TemplateMain), "template", "0.1", "Hythrex")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+namespace TemplateMod
+{
+	public class TemplateMain : BloonsTD6Mod
+    {
+		// dimeln
+
+
+		public override void OnMainMenu()
+		{
+			base.OnMainMenu();
+		}
+
+		public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("Template Mod Loaded!");
+        }
+    }
+}
