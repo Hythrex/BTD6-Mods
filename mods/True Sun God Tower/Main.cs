904462d408d558ef13b6904fcbcbaed1b2050624
Hythraux <86388177+Hythrex@users.noreply.github.com>
2022-08-03 13:53:57 -0400

    Add files via upload

---
+++ mods/True Sun God Tower/Main.cs
@@ -0,0 +1,20 @@
+﻿using BTD_Mod_Helper;
+using MelonLoader;
+using Harmony;
+using Assets.Scripts.Simulation.Towers;
+using Assets.Scripts.Models;
+
+[assembly: MelonInfo(typeof(TemplateMod.Main), "Your Mod Name", "1.0.0", "Your Name")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+namespace TemplateMod
+{
+    public class Main : BloonsTD6Mod
+    {
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            MelonLogger.Msg("ding");
+        }
+
+    }
+}
